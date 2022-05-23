edda74c32ae66928fd7e22aaca461f1eab8dd8dc
StasKalacheff <staskalacheff@yandex.ru>
2022-05-23 18:49:03 +0300

    На проверку. Ревью 1

--- src/Converter.java
+++ src/Converter.java
@@ -1,7 +1,7 @@
 public class Converter {
     double km = 0.00075;
     double kcal = 0.05;
-    double converterKm(int sumMonth) {
+    public double converterKm(int sumMonth) {
         return sumMonth * km;
     }
     public double converterKcal(int sumMonth) {
